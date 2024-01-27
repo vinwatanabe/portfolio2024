169068280582fc4d0483d5db4766fc1e0dc1c21c
Vinícius Watanabe <vinicius_watanabe@hotmail.com>
2024-01-27 13:11:58 -0800

    Fixed project page

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -73,9 +73,9 @@ const Project = () => {
 			</div>
 
 			{/* Header title */}
-			<div className='relative h-40'>
+			<div className='sm:relative sm:h-40'>
 				<div
-					className={`absolute translate-y-[-50%] left-[50%] translate-x-[-50%] bg-darkBlue px-5 py-10 sm:py-16 w-[90%] sm:w-[80%] lg:w-[60%]`}>
+					className={`relative sm:absolute sm:translate-y-[-50%] left-[50%] translate-x-[-50%] bg-darkBlue px-5 py-10 sm:py-16 w-[90%] sm:w-[80%] lg:w-[60%]`}>
 					<p className='text-orange text-section-title sm:text-main-title font-bebas text-center mb-3'>
 						Lorem ipsum dolor sit amet
 					</p>
